dd5bce07a13880d4682be63aa7f0806b59416a7d
Raul-Herrera-Gil <rh_gil@yahoo.es>
2023-05-02 20:40:44 +0200

    save con el codigo de contact.js más depurado, pendiente de enviar adjunto

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -7,14 +7,13 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 
-// Rest
-
-router.post('/contact', (req, res) => {
-
-    // Extraemos los datos del formulario de contacto
+// Ruta para la solicitud POST del formulario de contacto
+router.post('/contact', async (req, res) => {
+    try {
+        // Extraemos los datos del formulario
         const { nombre, email, asunto, mensaje } = req.body;
-    
-        // Configuración para el envío del correo
+        
+        // Configuramos el servicio de correo
         let jConfig = {
             service: 'gmail',
             host: 'smtp.gmail.com',
@@ -26,49 +25,54 @@ router.post('/contact', (req, res) => {
                 pass: `${process.env.EMAIL_PASSWORD_API}`
             }
         };
-    
-        /*
-        Definimos el JSON de datos que se debe crear para establecer la comunicación con el servidor de correos, 
-        en donde se incluyen los datos de los destinatarios, asunto, mensaje, etc..
-        */
-    
+        
+        // Creamos el objeto para enviar el correo
         let emailSend = {
-            from: "'PRoLocation'<prolocationtfg@gmail.com>", //remitente
-            to: 'rh_gil@yahoo.es', //destinatario
-            subject: 'Formulario de contacto', //asunto del correo
+            from: "'PRoLocation'<prolocationtfg@gmail.com>",
+            to: 'rh_gil@yahoo.es',
+            subject: 'Formulario de contacto',
             html: ` 
             <h1>Información del usuario</h1>
-            
             <p><b>Nombre:</b> ${nombre}</p>
             <p><b>Email:</b> ${email}</p>
             <p><b>Asunto:</b> ${asunto}</p>
             <br>
             <p>${mensaje}</p>
-    `}
-    
-        /*
-        Una vez hemos definido la configuración a utilizar y los datos del correo a enviar, 
-        creamos un objeto transportador a través del siguiente método "createTransport".
-        */
+            `
+        };
+        
+        // Creamos el objeto transportador para enviar el correo
         let createTransport = nodemailer.createTransport(jConfig);
+        
+        // Enviamos el correo y mostramos los detalles en la consola
 
-        //Este objeto crea una variable de transporte que se comunicara con el servidor SMTP y enviará el correo.
-    
-        createTransport.sendMail(emailSend, function (error, info) {
-            if (error) {
-                console.log("Error al enviar email");
-            } else {
-                console.log();
-                console.log("Correo enviado correctamente");
-                console.log();
-                console.log("* Nombre: " + `${JSON.stringify(req.body.nombre)}`);
-                console.log("* Email: " + `${JSON.stringify(req.body.email)}`);
-                console.log("* Asunto: " + `${JSON.stringify(req.body.asunto)}`);
-                console.log("* Mensaje: " + `${JSON.stringify(req.body.mensaje)}`);
-            }
-            createTransport.close();
-        });
-    });
+        /*Creamos la variable info para almacenar la respuesta del servidor 
+        contiene información como el estado del envío del correo electrónico, 
+        la hora de envío, el ID del mensaje y otros detalles adicionales por si lo necesitaramos en un futuro.
+        */
+        let info = await createTransport.sendMail(emailSend);
+
+        //Mostramos los datos en consola
+        console.log();
+        console.log("Correo enviado correctamente");
+        console.log();
+        console.log("* Nombre: " + `${JSON.stringify(req.body.nombre)}`);
+        console.log("* Email: " + `${JSON.stringify(req.body.email)}`);
+        console.log("* Asunto: " + `${JSON.stringify(req.body.asunto)}`);
+        console.log("* Mensaje: " + `${JSON.stringify(req.body.mensaje)}`);
+       
+        // Cerramos la conexión con el servidor de correo
+        createTransport.close();
+        
+        // Enviamos una respuesta HTTP 200 con el mensaje de éxito
+        res.status(200).send("Correo enviado correctamente");
+    } catch (error) {
+        // Mostramos los detalles del error en la consola y enviamos una respuesta HTTP 500
+        console.log("Error al enviar email", error);
+        res.status(500).send("Error al enviar email");
+    }
+});
+
 
 // Export
 
