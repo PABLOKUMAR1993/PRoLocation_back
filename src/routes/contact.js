46d3c0d4d928a719e39ab03a04a0bcd28bff505d
Raul-Herrera-Gil <rh_gil@yahoo.es>
2023-05-02 18:37:06 +0200

    Se ha cambiado la ruta y configuración de envio de correos de server.js a contact.js y funciona. Apendiente poder enviar archivos adjuntos

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -9,61 +9,67 @@ require('dotenv').config();
 
 // Rest
 
-router.post('/contact', async (req, res) => {
-    const { name, email, subject, message } = req.body;
+router.post('/contact', (req, res) => {
+
+    // Extraemos los datos del formulario de contacto
+        const { nombre, email, asunto, mensaje } = req.body;
     
-    console.log(name);
-    console.log(email);
-    console.log(subject);
-    console.log(message);
-    /*contentHTML = `
-        <h1>Información del usuario</h1>
-        <ul>
-            <li>Nombre: ${name}</li>
-            <li>Email: ${email}</li>
-            <li>Asunto: ${subject}</li>
-        </ul>
-        <p>${message}</p>
-    `;*/
-    //console.log(contentHTML);
+        // Configuración para el envío del correo
+        let jConfig = {
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'login',
+                user: `${process.env.EMAIL}`,
+                pass: `${process.env.EMAIL_PASSWORD_API}`
+            }
+        };
+    
+        /*
+        Definimos el JSON de datos que se debe crear para establecer la comunicación con el servidor de correos, 
+        en donde se incluyen los datos de los destinatarios, asunto, mensaje, etc..
+        */
+    
+        let emailSend = {
+            from: "'PRoLocation'<prolocationtfg@gmail.com>", //remitente
+            to: 'rh_gil@yahoo.es', //destinatario
+            subject: 'Formulario de contacto', //asunto del correo
+            html: ` 
+            <h1>Información del usuario</h1>
+            
+            <p><b>Nombre:</b> ${nombre}</p>
+            <p><b>Email:</b> ${email}</p>
+            <p><b>Asunto:</b> ${asunto}</p>
+            <br>
+            <p>${mensaje}</p>
+    `}
+    
+        /*
+        Una vez hemos definido la configuración a utilizar y los datos del correo a enviar, 
+        creamos un objeto transportador a través del siguiente método "createTransport".
+        */
+        let createTransport = nodemailer.createTransport(jConfig);
 
-    //Configuración del host
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'login',
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_PASSWORD_API
-        }
-
+        //Este objeto crea una variable de transporte que se comunicara con el servidor SMTP y enviará el correo.
+    
+        createTransport.sendMail(emailSend, function (error, info) {
+            if (error) {
+                console.log("Error al enviar email");
+            } else {
+                console.log();
+                console.log("Correo enviado correctamente");
+                console.log();
+                console.log("* Nombre: " + `${JSON.stringify(req.body.nombre)}`);
+                console.log("* Email: " + `${JSON.stringify(req.body.email)}`);
+                console.log("* Asunto: " + `${JSON.stringify(req.body.asunto)}`);
+                console.log("* Mensaje: " + `${JSON.stringify(req.body.mensaje)}`);
+            }
+            createTransport.close();
+        });
     });
 
-    //Verificación de envio
-    transporter.verify().then(() => {
-        console.log("Enviando email...");
-    }).catch(err => console.log(err));
-
-    //Método asincrono para envío de correo
-    const info = await transporter.sendMail({
-        from: "'PRoLocation'<prolocationtfg@gmail.com>",
-        to: 'rh_gil@yahoo.es',
-        subject: 'Formulario de contacto',
-        //html: contentHTML
-    })
-
-    console.log("Mensaje enviado", info.messageId);
-    res.send("El correo ha sido enviado correctamente");
-
-});
-attachments: [
-    {
-      filename: '1682274239051 - 52714.jpg',
-      path: '../uploads'
-    }
-  ]
 // Export
 
 module.exports = router;
